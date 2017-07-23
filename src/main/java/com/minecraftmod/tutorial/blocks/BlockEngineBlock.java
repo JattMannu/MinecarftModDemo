8248fef75c77df1bda3c1688b5c49f10fb448ffe
JattMannu <manpreet_firewall@yahoo.com.sg>
2017-07-23 17:42:38 +0800

    Tested our on OnEvent Methods 1. onBlockActivated 2. onBlockClicked 3. onBlockDestroyedByPlayer 	1. Place a item once the item is destroyed.

--- src/main/java/com/minecraftmod/tutorial/blocks/BlockEngineBlock.java
+++ src/main/java/com/minecraftmod/tutorial/blocks/BlockEngineBlock.java
@@ -4,12 +4,39 @@ package com.minecraftmod.tutorial.blocks;
 import com.minecraftmod.tutorial.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockEngineBlock extends Block {
 
     public BlockEngineBlock() {
         super(Material.ANVIL);
+        super.setHardness(1);
         setUnlocalizedName(Reference.MechBlocks.ENGINEBLOCK.getUnlocalizedName());
         setRegistryName(Reference.MechBlocks.ENGINEBLOCK.getRegistryName());
     }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        System.out.println("BlockEngineBlock's onBlockActivated trigger");
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+        super.onBlockClicked(worldIn, pos, playerIn);
+        System.out.println("BlockEngineBlock's onBlockClicked trigger");
+    }
+
+    @Override
+    public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+        super.onBlockDestroyedByPlayer(worldIn, pos, state);
+        this.dropBlockAsItem(worldIn, pos, state , 1);
+        System.out.println("BlockEngineBlock's onBlockDestroyedByPlayer trigger");
+    }
+
 }
